92bc249400ceca877bf0be5ea015a9b317142bff
AndresBlandonW <andblandon@hotmail.com>
2021-11-11 13:03:45 -0800

    Update imports

--- console.py
+++ console.py
@@ -3,7 +3,10 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
-
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.city import City
 from models.engine.file_storage import FileStorage
 from models import storage
 
